8c33856d35341507435b4536b3dfd8777ec61fcf
yuval <yuvald@gigaspaces.com>
2018-01-28 17:45:29 +0200

    Added more threads

--- src/main/java/org/apache/ignite/benchmark/Multithredingbenchmark.java
+++ src/main/java/org/apache/ignite/benchmark/Multithredingbenchmark.java
@@ -12,23 +12,50 @@ import java.util.Set;
 public class Multithredingbenchmark {
 
     public static void main(String[] args){
-        Map<Long, MyPerson> personMap = DataBuilder.buildData(0L,100L);
-        WriteThread writeThread = new WriteThread(personMap);
 
-        Set<Long> keys = DataBuilder.buildKeys(0L,100L);
-        ReadThread readThread = new ReadThread(keys);
+        Map<Long, MyPerson> personMap1 = DataBuilder.buildData(0L,10000L);
+        WriteThread writeThread1 = new WriteThread(personMap1);
+        Map<Long, MyPerson> personMap2 = DataBuilder.buildData(20000L,30000L);
+        WriteThread writeThread2 = new WriteThread(personMap2);
+        Map<Long, MyPerson> personMap3 = DataBuilder.buildData(40000L,60000L);
+        WriteThread writeThread3 = new WriteThread(personMap3);
+        Map<Long, MyPerson> personMap4 = DataBuilder.buildData(70000L,80000L);
+        WriteThread writeThread4 = new WriteThread(personMap4);
+
+        Set<Long> keys1 = DataBuilder.buildKeys(0L,10000L); //exist
+        ReadThread readThread1 = new ReadThread(keys1);
+        Set<Long> keys2 = DataBuilder.buildKeys(10000L,20000L); //not exist
+        ReadThread readThread2 = new ReadThread(keys2);
+        Set<Long> keys3= DataBuilder.buildKeys(25000L,35000L); // half exist and half not
+        ReadThread readThread3 = new ReadThread(keys3);
+        Set<Long> keys4 = DataBuilder.buildKeys(40000L,50000L);
+        ReadThread readThread4 = new ReadThread(keys4);
+
 
         Pair<String, String> operationPair = new Pair<String, String>("Operation", "Multithreding Write Read");
         Pair<String, Long> ObjNumberPair = new Pair<String, Long>("Obj Number", 100L);
         Long start = System.currentTimeMillis();
 
-        writeThread.start();
-        readThread.start();
+        writeThread1.start();
+        readThread1.start();
+        writeThread2.start();
+        readThread2.start();
+        writeThread3.start();
+        readThread3.start();
+        writeThread4.start();
+        readThread4.start();
 
-        // wait until both threads finished
+        // wait until all threads are finished
         try {
-            writeThread.join();
-            readThread.join();
+            writeThread1.join();
+            readThread1.join();
+            writeThread2.join();
+            readThread2.join();
+            writeThread3.join();
+            readThread3.join();
+            writeThread4.join();
+            readThread4.join();
+
         }catch(InterruptedException e){
             e.printStackTrace();
         }
